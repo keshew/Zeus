c8bf5de8f3e7efbbdeb0da6995081169555ddd27
keshew <artyom.korotckov2017@yandex.ru>
2025-02-07 02:57:12 +0500

    Initial Commit

---
+++ Zeus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zeus
+//
+//  Created by Артём Коротков on 07.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zeus/ZeusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeusApp.swift
+//  Zeus
+//
+//  Created by Артём Коротков on 07.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ZeusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
